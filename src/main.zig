381f52552d3a36ca435175cda51ccab38ca0911e
Herman Gohn <gohnherman@gmail.com>
2024-06-12 23:14:07 +0200

    test

--- src/main.zig
+++ src/main.zig
@@ -62,9 +62,7 @@ const http = struct {
                 for (headers) |header| {
                     bytes = try arrayConcatu8(&[_][]const u8{ bytes, header.toBytes() });
                 }
-                return try arrayConcatu8(&[_][]const u8{ bytes, "\r\n" }) catch |_| {
-                    return []const u8;
-                };
+                return try arrayConcatu8(&[_][]const u8{ bytes, "\r\n" }) catch []const u8;
             } else {
                 return []const u8;
             };
